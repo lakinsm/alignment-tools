399186564823546a1f39f314beb387afca7d9b5c
lakinsm <steven.m.lakin@gmail.com>
2021-04-01 12:57:44 -0500

    Metadata standalone script nanopore

--- plot_nanopore_metadata.py
+++ plot_nanopore_metadata.py
@@ -80,7 +80,7 @@ def parse_throughfile(infile):
 			bcr_pass = int(entries[2])
 			bcr_fail = int(entries[3])
 			bcbp = int(entries[8])
-			ret += (mintime, bcr_pass, bcr_fail, bcbp)
+			ret += ((mintime, bcr_pass, bcr_fail, bcbp),)
 			line = f.readline()
 	return ret
 
