b0a2465faeed5e93173fede7c8f2c42018ee5109
lakinsm <steven.m.lakin@gmail.com>
2021-04-12 15:46:15 -0500

    Dir walk testing

--- nanopore_dir_walk_backup.sh
+++ nanopore_dir_walk_backup.sh
@@ -17,5 +17,5 @@ module load lakinsm-alignment-tools
 
 echo -e "\n$(date): Starting backup of folder $1 to $2\n"
 this_datetime=$( date +"%FT%H%M" )
-srun python3 nanopore_dir_walk_backup.py "$1" "$2" --logfile "/lustrefs/data/backup_logs/${this_datetime}_python_script.log" --slurm
+srun nanopore_dir_walk_backup.py "$1" "$2" --logfile "/lustrefs/data/backup_logs/${this_datetime}_python_script.log" --slurm
 echo -e "\n$(date): Backup finished for folder $1 to $2\n"
