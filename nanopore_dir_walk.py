58bdab7faa0bb8a42f63f233053f6177b314a5ee
lakinsm <steven.m.lakin@gmail.com>
2021-04-12 09:22:40 -0500

    Dir walk testing

--- nanopore_dir_walk.py
+++ nanopore_dir_walk.py
@@ -4,9 +4,10 @@ import sys
 import os
 import fnmatch
 import argparse
+import datetime
 
 if sys.version_info[0] < 3:
-	raise Exception('Python 3.5 or a more recent version is required.')
+	raise Exception('Python 3 or a more recent version is required.')
 
 
 def find_fastq_pass(root_dir):
@@ -23,6 +24,36 @@ def find_fastq_pass(root_dir):
 	return fq_pass
 
 
+def check_file_match(root_source, root_dest, fq_pass, write_text_log=None):
+	"""
+	Compare FASTQ and Nanopore flowcell metadata files from source to destination, and update destination files if
+	not matching from source.  This is a destructive/overwrite operation that uses a temporary intermediate file.
+	This operation will never delete existing data (if it is absent from source but present in destination).  A logfile
+	is optionally written by default with date/time of file updates or status checks.
+	:param root_source: STR, absolute directory path to instrument-level root directory for source files
+	:param root_dest: STR, absolute directory path to instrument-level root directory for destination files
+	:param fq_pass: TUPLE, an array of absolute paths to all fastq_pass directories in the source root directory
+	:param write_text_log: NONE/STR, write a tab-delimited log of date/time, source filepath, dest filepath, and status
+	:return: None
+	"""
+	log_handle = None
+	if write_text_log:
+		log_handle = open(write_text_log, 'a')
+		log_handle.write('\n\n{}\tBegin file checking and copy, source: {}, destination: {}\n'.format(
+			datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+			root_source,
+			root_dest
+		))
+
+	for fq_path in fq_pass:
+		for _, _, filenames in os.walk(fq_path):
+			for f in filenames:
+				print(f)
+
+	if log_handle:
+		log_handle.close()
+
+
 parser = argparse.ArgumentParser('nanopore_dir_walk.py')
 parser.add_argument('root_dir', default=None, type=str,
                     help='Root instrument directory to check for updated files')
@@ -33,4 +64,4 @@ parser.add_argument('dest_dir', default=None, type=str,
 if __name__ == '__main__':
 	args = parser.parse_args()
 	fq_pass_paths = find_fastq_pass(os.path.realpath(args.root_dir))
-	print(fq_pass_paths)
+	check_file_match(args.root_dir, args.dest_dir, fq_pass_paths)
