7c82c521c48ca40a2fd3915ac8f84125345337f8
lakinsm <steven.m.lakin@gmail.com>
2021-05-26 13:59:25 -0400

    dir walk backup

--- nanopore_dir_walk_backup.sh
+++ nanopore_dir_walk_backup.sh
@@ -9,5 +9,5 @@
 
 echo -e "\n$(date): Starting backup of folder $1 to $2\n"
 this_datetime=$( date +"%FT%H%M%S" )
-/cm/local/apps/python37/bin/python3 /home/software/nbaf/github_repos/alignment-tools/nanopore_dir_walk_backup.py "$1" "$2" --logfile "/lustrefs/data/backup_logs/${this_datetime}_python_script.log" -n
+/cm/local/apps/python37/bin/python3 nanopore_dir_walk_backup.py "$1" "$2" --logfile "${this_datetime}_python_script.log" -n
 echo -e "\n$(date): Backup finished for folder $1 to $2\n"
